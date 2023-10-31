94fcd3adb70113e2d738e507999e5190f4278b46
Vikram Bhattacharjee <53374818+vbhattaccmu@users.noreply.github.com>
2023-10-31 17:19:15 +0800

    feat: add config and peer

---
+++ src/config.rs
@@ -0,0 +1,101 @@
+use crate::peer::Peer;
+use libp2p::identity::{Keypair, ed25519};
+
+/// Configuration that specifies parameters such as network connection, node identification,
+/// message buffer sizes and interval.
+///
+/// For convenience, default values are predefined when calling `new()` or `from_keypair()`
+/// as following:
+/// - Port number: 25519
+/// - Buffer size of send commands: 8
+/// - Buffer size of message: 10
+/// - Buffer size of broadcast messages: 10
+/// - Interval for peer discover: 10 secs
+#[derive(Clone)]
+pub struct Config {
+    /// keypair used for identification of this network node
+    pub(crate) keypair: Keypair,
+
+    /// port number for TCP connection
+    pub port: u16,
+
+    /// bootstrap peers for initial connection setup
+    pub boot_nodes: Vec<Peer>,
+
+    /// buffer size of commands initiated from caller
+    pub send_command_buffer_size: usize,
+
+    /// buffer size of message that is sent to this network node.
+    pub private_msg_buffer_size: usize,
+
+    /// buffer size of broadcast messages.
+    pub broadcast_msg_buffer_size: usize,
+
+    /// Interval in seconds for querying networking to discover peers.
+    pub peer_discovery_interval: u64,
+}
+
+impl Config {
+    /// Create config with an automatically generated keypair.
+    pub fn new() -> Self {
+        Self {
+            keypair: Keypair::generate_ed25519(),
+            port: 25519,
+            boot_nodes: Vec::new(),
+
+            send_command_buffer_size: 8,
+            private_msg_buffer_size: 10,
+            broadcast_msg_buffer_size: 10,
+            peer_discovery_interval: 10, // secs
+        }
+    }
+
+    /// Create config with an existing keypair.
+    pub fn new_with_keypair(mut keypair_bytes: Vec<u8>) -> Self {
+        Self {
+            keypair: ed25519::Keypair::try_from_bytes(&mut keypair_bytes).expect("Invalid ed25519 keypair").into(),
+            port: 25519,
+            boot_nodes: Vec::new(),
+            send_command_buffer_size: 8,
+            private_msg_buffer_size: 10,
+            broadcast_msg_buffer_size: 10,
+            peer_discovery_interval: 10, // secs
+        }
+    }
+
+    /// Set the port number used for networking
+    pub fn set_port(mut self, port: u16) -> Self {
+        self.port = port;
+        self
+    }
+
+    /// buffer size of commands initiated from caller
+    pub fn set_send_command_buffer_size(mut self, send_command_buffer_size: usize) -> Self {
+        self.send_command_buffer_size = send_command_buffer_size;
+        self
+    }
+
+    /// buffer size of message that is sent to this network node.
+    pub fn set_private_msg_buffer_size(mut self, private_msg_buffer_size: usize) -> Self {
+        self.private_msg_buffer_size = private_msg_buffer_size;
+        self
+    }
+
+    /// buffer size of broadcast messages.
+    pub fn set_broadcast_msg_buffer_size(mut self, broadcast_msg_buffer_size: usize) -> Self {
+        self.broadcast_msg_buffer_size = broadcast_msg_buffer_size;
+        self
+    }
+
+    /// Interval in seconds for querying networking to discover peers.
+    pub fn set_peer_discovery_interval(mut self, peer_discovery_interval: u64) -> Self {
+        self.peer_discovery_interval = peer_discovery_interval;
+        self
+    }
+
+    /// bootstrap nodes for initial connection
+    pub fn set_boot_nodes(mut self, boot_nodes: Vec<Peer>) -> Self {
+        self.boot_nodes = boot_nodes;
+        self
+    }
+}
---
+++ src/peer.rs
@@ -0,0 +1,33 @@
+use std::net::Ipv4Addr;
+
+use libp2p::{PeerId, identity::{ed25519, PublicKey}};
+use crate::PublicAddress;
+
+/// Peer consists of required information to identify an entity in the network, such as
+/// Peer Id, IPv4 Address and port number.
+#[derive(Clone)]
+pub struct Peer {
+    /// Peer id in the p2p network
+    pub peer_id: PeerId,
+
+    /// IP address (v4) of connection
+    pub ip_address: Ipv4Addr,
+
+    /// port number of connection
+    pub port: u16,
+}
+
+impl Peer {
+    /// Instantiation of PeerInfo. It is used in bootstrap nodes in [crate::configuration::Config].
+    ///
+    /// ## Panics
+    /// Panics if address is not a valid Ed25519 public key.
+    pub fn new(address: PublicAddress, ip_address: Ipv4Addr, port: u16) -> Self {
+        let ed25519_pk: PublicKey = ed25519::PublicKey::try_from_bytes(&address).unwrap().into();
+        Self {
+            peer_id: ed25519_pk.to_peer_id(),
+            ip_address,
+            port,
+        }
+    }
+}
